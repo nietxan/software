c20a1a191ba46c67ed0d78c1ec5dc7e93802be54
Niyetkhan <niethan.2004@gmail.com>
2023-12-13 16:10:47 +0600

    another issue fixing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module edu.software.Application {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires org.jetbrains.annotations;
 
     opens edu.software.application to javafx.fxml;
     exports edu.software.application;
